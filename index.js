0c05904219d791db245417f645d84d6fac8b677f
Zbynek Roubalik <zroubalik@gmail.com>
2023-01-20 22:49:29 +0100

    change
    Signed-off-by: Zbynek Roubalik <zroubalik@gmail.com>

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const handle = async (context, body) => {
   } else if (context.method === 'GET') {
   // If the request is an HTTP GET, the context will include a query string, if it exists
   return {
-      query: "Hello!",
+      query: "Hello world!",
     }
   } else {
     return { statusCode: 405, statusMessage: 'Method not allowed' };
